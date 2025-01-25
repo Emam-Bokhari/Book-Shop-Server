1b4fee40c662699914bc72c99efdf4cf360f3a5d
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-25 20:13:26 +0600

    feat(order): add routes for order management

--- src/app/modules/Order/order.route.ts
+++ src/app/modules/Order/order.route.ts
@@ -3,6 +3,12 @@ import { OrderControllers } from "./order.controller";
 
 const router = express.Router();
 
-router.post("/", OrderControllers.createOrder)
+router.post("/", OrderControllers.createOrderController);
+
+router.get("/", OrderControllers.getAllOrdersController);
+
+router.get("/:id", OrderControllers.getOrderController);
+
+router.patch("/:id/status", OrderControllers.updateOrderStatusController);
 
 export const OrderRoutes = router;
