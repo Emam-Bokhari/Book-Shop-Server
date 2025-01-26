6f77f6fb7720b78dfcfffab5e07a4e36721d4b4a
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-26 18:59:29 +0600

    feat(validation): add Zod validation for creating and updating shopping address

--- src/app/modules/ShippingAddress/shippingAddress.route.ts
+++ src/app/modules/ShippingAddress/shippingAddress.route.ts
@@ -1,16 +1,18 @@
 import express from 'express';
 import { ShippingAddressControllers } from './shippingAddress.controller';
+import { validateRequestSchema } from '../../middlewares/validateRequestSchema';
+import { ShippingAddressValidationSchema } from './shippingAddress.validation';
 
 const router = express.Router();
 
-router.post('/', ShippingAddressControllers.createShippingAddressController);
+router.post('/', validateRequestSchema(ShippingAddressValidationSchema.createShippingAddressValidationSchema), ShippingAddressControllers.createShippingAddressController);
 
 router.get('/', ShippingAddressControllers.getAllShippingAddressController);
 
 router.get('/:id', ShippingAddressControllers.getShippingAddressController);
 
 router.patch(
-  '/:id',
+  '/:id', validateRequestSchema(ShippingAddressValidationSchema.updateShippingAddressValidationSchema),
   ShippingAddressControllers.updatedShippingAddressController,
 );
 
