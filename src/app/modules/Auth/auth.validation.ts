fe8b928a0944c19296e2d76ed6bd384af18e2d66
Moshfiqur Rahman <studentemam@gmail.com>
2025-01-26 20:21:13 +0600

    feat(authentication): add Zod validation schema for login

---
+++ src/app/modules/Auth/auth.validation.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+const loginValidationSchema = z.object({
+    body: z.object({
+        email: z.string().email().trim(),
+        password: z.string().trim(),
+    })
+})
+
+export const AuthValidationSchema = {
+    loginValidationSchema,
+}
