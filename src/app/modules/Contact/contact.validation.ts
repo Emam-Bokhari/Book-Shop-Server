702f7ef9c0b513d1dffd3fd20dd01a9617cc0c75
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-28 15:48:42 +0600

    feat: add validation schema for creating contacts

---
+++ src/app/modules/Contact/contact.validation.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+const createContactValidationSchema = z.object({
+    body: z.object({
+        name: z.string(),
+        email: z.string().email('Invalid email format'),
+        message: z.string(),
+        isDeleted: z.boolean().default(false),
+    }),
+});
+
+export const ContactValidationSchema = {
+    createContactValidationSchema,
+};
