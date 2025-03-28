4dd63247f201214a0b8cbb3ea968e2987a3c7198
Moshfiqur Rahman <studentemam@gmail.com>
2025-03-28 19:42:00 +0600

    feat(newsletter): add Newsletter model with soft delete support

--- src/app/modules/Newsletter/newsletter.interface.ts
+++ src/app/modules/Newsletter/newsletter.interface.ts
@@ -1,3 +1,4 @@
 export type TNewsletter = {
     email: string;
+    isDeleted?: boolean;
 };
---
+++ src/app/modules/Newsletter/newsletter.model.ts
@@ -0,0 +1,30 @@
+import { model, Schema } from 'mongoose';
+import { TNewsletter } from './newsletter.interface';
+import { excludeDeletedAggregation, excludeDeletedQuery } from '../../utils/moduleSpecific/queryFilters';
+
+
+const newsletterSchema = new Schema<TNewsletter>(
+    {
+        email: {
+            type: String,
+            required: true,
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    },
+);
+
+// query middleware for soft delete by utils
+newsletterSchema.pre('find', excludeDeletedQuery);
+newsletterSchema.pre('findOne', excludeDeletedQuery);
+
+// aggregate middleware for soft delete by utils
+newsletterSchema.pre('aggregate', excludeDeletedAggregation);
+
+export const NewsLetter = model<TNewsletter>('Newsletter', newsletterSchema);
